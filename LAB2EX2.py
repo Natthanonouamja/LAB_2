c823aad27b72f8fa647fbab7f150e1d10fd3a4b9
Teeradon <118620558+Natthanonouamja@users.noreply.github.com>
2022-11-19 22:33:02 +0700

    LAB2

---
+++ LAB2EX2.py
@@ -0,0 +1,6 @@
+array = ["Number", "ID", "Name", "Count"]
+print(array)
+print(len(array))
+print(array.index("Name"))
+array.append("Status")
+print(array)
---
+++ LAB2EZ1.py
@@ -0,0 +1,7 @@
+array = [5, 7, 9, 11, 13]
+lenarray = len(array)
+sumarray = sum(array)
+
+print(array)
+print(lenarray)
+print(sumarray)
